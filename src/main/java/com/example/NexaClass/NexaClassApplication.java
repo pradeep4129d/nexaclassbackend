6b14d50e4b62e7b49b6c80369131c8879aab6ffc
pradeep4129d <bolla.h678@gmail.com>
2025-08-03 11:31:47 +0530

    first

---
+++ src/main/java/com/example/NexaClass/NexaClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.NexaClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexaClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexaClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NexaClass/NexaClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NexaClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexaClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
